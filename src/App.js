ad09843ff58db4d270b7d915594d9012935ef474
Marcos Rivereto <mrivereto@gmail.com>
2020-07-31 16:21:39 -0300

    Completando o desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+
   async function handleAddRepository() {
-    // TODO
+    api.post('/repositories', {
+      title:  `Oministack ${new Date().getTime()}`,
+      url: "http://gihub.com/mrivereto",
+      techs: ["NodeJS", "React"]
+    }).then(response => {
+      setRepositories([...repositories, response.data]);
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    console.log(id);
+    api.delete(`/repositories/${id}`).then(response => {
+      const result = repositories.filter(function (item) { return item.id !== id });
+      setRepositories([...result]);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+            </li>
+          )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
